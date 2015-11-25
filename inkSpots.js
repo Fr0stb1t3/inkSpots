23ca11ae7e5fe92391ee0811223f146a629351a8
Antoni Atanasov <antonikliment@gmail.com>
2015-11-25 21:34:48 +0200

    Code cleanup + Canvas adjustment
    Replaced the temporary canvas element, with a canvas used only in the
    script

--- inkSpots.js
+++ inkSpots.js
@@ -1,5 +1,5 @@
 /**
-    InkSpots 0.0.1 (prototype version)
+    InkSpots 0.0.3 (prototype version)
     InkSpots generates random spotted background
     Copyright (c) 2015 Antoni Atanasov
     License: MIT 
@@ -10,73 +10,60 @@
 var window = window;
 var spotDefs = typeof spotDefs === 'undefined' ? 0 : spotDefs;
 (function() {
-    var testCounter= 0;
-    console.log(spotDefs);
-    var globalImageHolder={};//temporary storage
+    "use strict";
    
-    function randomIntFromInterval(min,max,space)
-    {
-        var space = space || 0;
-        return Math.floor(Math.random()*(max-min+1)+min) - space;
+    
+    window.applyInkSpots = function(target,density) {
+        var density = density || 10;
+        var elements=document.querySelectorAll(target);
+        for(var j = 0 ;j< elements.length; j++){
+            var el = elements[j];
+            _generateBackground(el,density);
+        }   
     }
-    window.applyInkSpots = function(target) {
-        applyCanvasBackground(target);
-    }
-    function applyCanvasBackground(target){
-        var c = document.getElementById("testCanvs");
-        c.setAttribute("width", "1300px");
-        c.setAttribute("height", "1300px");
+   
+    function _generateBackground(el,density){
+        var c = document.createElement('canvas');
+        c.setAttribute("width", el.offsetWidth+"px");
+        c.setAttribute("height", el.offsetHeight+"px");
+        c.crossOrigin = "Anonymous";
         var ctx = c.getContext("2d");
         ctx.clearRect(0, 0, c.width, c.height);
-        drawObjectSet( ctx, spotDefs, 10,
-        function(){
-            var elements=document.querySelectorAll(target);
-            for(j = 0 ;j< elements.length; j++){
-                var dataURL;
-                  c.crossOrigin = "Anonymous";
-                try {
-                     dataURL = c.toDataURL("image/png");
-                }catch(err){
-                    dataURL="";
-                }
-                var el = elements[j];
-                var bPos = "background-position: 0px 0px;";
-                var bRep = "background-repeat: no-repeat;";
-                var bSize = "background-size: 100%;";
-                el.setAttribute('style', 'background-image: url('+dataURL+');'+bPos+bRep+bSize)
-                c.setAttribute('style','display:none');
-            }
-        });
+        drawObjectSet( ctx, spotDefs, density);
+        var dataURL;
+        try {
+            dataURL = c.toDataURL("image/png");
+        }catch(err){
+            dataURL="";
+        }
+        dSetBackground(el, dataURL );
     }
-    function drawObjectSet(canvas , objects, amount, _callback){
+    
+    function drawObjectSet(ctx , objects, amount, _callback){
         var taskCounter = amount;
         var completeCallbackTimer;
         if(objects instanceof Array ){
             taskCounter = amount*objects.length;
-            completeCallbackTimer = setInterval(checkComplete, amount  * 25);
             for(var i = 0;i < objects.length;i++){
-                objToCanvas( canvas,objects[i],amount,function(){taskCounter-=amount;} );
+                objToCanvas( ctx,objects[i],amount );
             }
         }else{
-            completeCallbackTimer = setInterval(checkComplete, amount*25);
-            objToCanvas( canvas,objects,amount,function(){taskCounter-=amount;} );
+            objToCanvas( ctx,objects,amount );
         }
-        function checkComplete(){
-            if(taskCounter > 0){//Left for debugging
-            }else{
-                 if (typeof _callback !== 'undefined') {
-                     clearTimeout(completeCallbackTimer);
-                    _callback();
-                }
-            }
+        if (typeof _callback !== 'undefined') {
+             clearTimeout(completeCallbackTimer);
+            _callback();
         }
     }
     function objToCanvas( ctx, shapeObj, amount, _callback ){
+        var sizeWidth = ctx.canvas.width;//get Pseudo canvas dimensions
+        var sizeHeight = ctx.canvas.height;
+        
         for(var i = 0;i < amount;i++){
-            var h = randomIntFromInterval(.01,2);
+            var h = randomIntFromInterval(0.01,2);
             var w = h;
-            var x = randomIntFromInterval(-200,500);
-            var y = randomIntFromInterval(-200,500); 
+            var x = randomIntFromInterval(-(sizeWidth*1.2),sizeWidth*1.2);
+            var y = randomIntFromInterval(-(sizeHeight*1.2),sizeHeight*1.2); 
             shapeObj(ctx , x, y, h, w, 'rgba(0,0,0,.5)');
         }
         if (typeof _callback !== 'undefined') {
@@ -84,54 +71,24 @@ var spotDefs = typeof spotDefs === 'undefined' ? 0 : spotDefs;
         }
     }
 
-    function msieversion() {
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))
-            return true;
-        else
-            return false;
-    }
-    function safariversion(){
-        var ua = navigator.userAgent.toLowerCase(); 
-         if (ua.indexOf('safari') != -1) { 
-            if (ua.indexOf('chrome') > -1) {
-              return false; // Chrome
-            } else {
-              return true; // Safari
-            }
-          }
-    }
-
-
     if ( typeof jQuery !== 'undefined' ){
-        jQuery.fn.inkSpots = function() {
+        jQuery.fn.inkSpots = function(density) {
+            var density = density || 10;
             return this.each(function(i) {
-                    var el =this;
-                    var c = document.getElementById("testCanvs");
-                    c.setAttribute("width", "1300px");
-                    c.setAttribute("height", "1300px");
-                    var ctx = c.getContext("2d");
-                    
-                    ctx.clearRect(0, 0, c.width, c.height);
-                    drawObjectSet( ctx, spotDefs, 10,
-                    function(){
-                        var dataURL;
-                          c.crossOrigin = "Anonymous";
-                        try {
-                             dataURL = c.toDataURL("image/png");
-                        }catch(err){
-                            dataURL="";
-                        }
-                        var bPos = "background-position: 0px 0px;";
-                        var bRep = "background-repeat: no-repeat;";
-                        var bSize = "background-size: 100%;";
-                        el.setAttribute('style', 'background-image: url('+dataURL+');'+bPos+bRep+bSize)
-                        
-                        c.setAttribute('style','display:none');
-                        
-                    });
+                var el =this;
+                _generateBackground(el,density);
             })
         }
     };
+    function dSetBackground(el, bUrl){
+        var bPos = "background-position: 0px 0px;";
+        var bRep = "background-repeat: no-repeat;";
+        var bSize = "background-size: 100%;";
+        el.setAttribute('style', 'background-image: url('+bUrl+');'+bPos+bRep+bSize);
+    }
+    
+    function randomIntFromInterval(min,max)
+    {
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
 })();
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ Micro script that generates ink splatter type backgrounds based on canvas paths 
 The script works by generating a background image on a canvas and then converting that to a png data applied to the target elements
 
 ### Version
-0.0.2
+0.0.3
 
 
 # Usage
